1ead478eb42a61f6d5a5d91d1ed3dc7370f92a79
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-03-20 15:19:30 -0600

    Created gardenboxRU and set up the gradle files and linked JPA proj dependency

---
+++ GardenBoxRU/src/main/java/com/skilldistillery/jpacrud/GardenBoxRuApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenBoxRuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenBoxRuApplication.class, args);
+	}
+
+}

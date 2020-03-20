cf888109518b064c33a37606b0b38eec6de520c1
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-03-20 14:27:32 -0600

    Create JPA project and stub Garden entity and test case

---
+++ JPAGarden/src/main/java/com/skilldistillery/jpacrud/entities/Plant.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.jpacrud.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Plant {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	
+	
+	
+	public Plant() {
+		super();
+	}
+	public Plant(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Plant [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+
+	
+	
+
+}

a2a76ce1fa507c0c9449b2f938c50bd2199dce63
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-03-23 11:04:45 -0600

    Updated gradle files to optimize for server upload

--- GardenBoxRU/src/main/java/com/skilldistillery/jpacrud/GardenBoxRuApplication.java
+++ GardenBoxRU/src/main/java/com/skilldistillery/jpacrud/GardenBoxRuApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GardenBoxRuApplication {
+public class GardenBoxRuApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GardenBoxRuApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardenBoxRuApplication.class, args);
